8cee32210071b6df0a975cdc753d53238117bad9
Pogobanane <aenderboy@gmx.de>
2022-11-30 12:44:02 +0000

    add dependencies from run.sh

--- flake.nix
+++ flake.nix
@@ -14,10 +14,42 @@
       # doc: https://nixos.org/manual/nixpkgs/stable/#sec-pkgs-mkShell
       default = pkgs.mkShell {
         buildInputs = with pkgs; [ # also called `buildInputs`
-          #zlib # whatever libraries and runtime apps your application needs
+          # manually found (some used to be installed by random scripts)
           wrk
           nginx
-          # Find more packages on https://search.nixos.org/packages
+
+          # from run.sh
+          #git
+          #clang
+          #bsdcpio # not in nixpkgs. Maybe try gnu pkgs.cpio
+          doxygen
+          libhugetlbfs
+          #build-essential # whatever. maybe later
+          qemu
+          redis # actually redis-tools, but maybe this is enough
+          socat
+          meson
+          cscope
+          libseccomp # -dev
+          #uuid-runtime # what even is this
+          cloc
+          #uuid-dev # probably not needed
+          libuuid # actually libuuid1
+          bridge-utils
+          nettools
+          #ifupdown # we surely need this
+          bison
+          #curl
+          flex
+          #wget
+          #sudo
+          #libmhash-dev
+          libmhash # actually libmhash2
+          #linux-cpupower # whatever
+          gawk
+          #musl-tools
+          qemu-utils
+          # plot script requirements from run.sh omitted
         ];
 
         # All assignments become environments variable in the development shell.
